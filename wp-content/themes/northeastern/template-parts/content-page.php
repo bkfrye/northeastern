4839c2142a6f6e05ecdb90bfb3ece6dffb414ab6
Brandon <brandonkfrye@gmail.com>
2016-08-08 17:04:32 -0400

    modified styles for btn

--- wp-content/themes/northeastern/template-parts/content-page.php
+++ wp-content/themes/northeastern/template-parts/content-page.php
@@ -8,46 +8,64 @@
  */
 ?>
 
-<article>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<section class="banner_interior" style="background-image:url(http://northeastern.dev/wp-content/uploads/2016/08/unspecified-1.jpg)">
+	<div class="banner_content">
+		<div class="page_title">
+			<h1>Page Title</h1>
+		</div>
+	</div>
+</section>
 
-	<?php twentysixteen_post_thumbnail(); ?>
+<section class="content">
+	<article>
+		<?php the_content(); ?>
+	</article>
+	<aside>
+		<p>
+		</p>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+	</aside>
 
-		wp_link_pages( array(
-			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-			'after'       => '</div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+</section>
 
+<section class="img_gallery">
 	<?php
-		edit_post_link(
-			sprintf(
-				/* translators: %s: Name of current post */
-				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-				get_the_title()
-			),
-			'<footer class="entry-footer"><span class="edit-link">',
-			'</span></footer><!-- .entry-footer -->'
-		);
+		get_template_part( 'template-parts', 'freewall' );
 	?>
-
-</article><!-- #post-## -->
+</section>
 
 <section class="blog">
 	<?php
-	while ( have_posts() ) : the_post();
-		get_template_part( 'template-parts/blog', 'footer' );
-	endwhile;
+		// get_template_part( 'template-parts/blog', 'footer' );
 	?>
+
+
+	<div class="view_more">
+		<a href="#" class="btn-red">View More</a>
+	</div>
+</section>
+
+<section class="signups">
+	<div class="stay_connected">
+		<h3><span class="bold">Stay connected</span> and share your successes.</h3>
+		<input type="text" name="name" value="">
+
+	</div>
+	<div class="contribute">
+		<div>
+			<h3>Become a Contributor</h3>
+			<p>A stronger, more connected alumni network benefits all of us. What brilliance can you share?</p>
+		</div>
+		<div>
+			<a href="#" class="btn">Connect Now</a>
+		</div>
+
+	</div>
+</section>
+
+<section class="contact_person">
+	<article>
+
+	</article>
+
 </section>

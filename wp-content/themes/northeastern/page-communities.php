c85028abe8454e93373559c97a5460e3f4fdaa7b
Brandon <brandonkfrye@gmail.com>
2016-08-15 16:25:39 -0400

    created template

---
+++ wp-content/themes/northeastern/page-communities.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<section id="events">
+			<ul>
+			<?php
+				$args = array(
+					'post_type' => 'communities',
+					'orderby' => 'title',
+					'order' => 'ASC'
+				);
+
+				$communities = new WP_Query( $args );
+				while ( $communities->have_posts() ) : $communities->the_post();
+
+
+				echo '<li>'.get_the_title().'</li>';
+
+				endwhile;
+			?>
+			</ul>
+		</section>
+	</main>
+</div>
+
+<?php get_footer(); ?>

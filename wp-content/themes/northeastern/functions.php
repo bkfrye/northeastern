547facce0c835ff4f4ec402a9124a0375b0eff36
Brandon <brandonkfrye@gmail.com>
2016-08-31 18:28:56 -0400

    updated favicon directory

--- wp-content/themes/northeastern/functions.php
+++ wp-content/themes/northeastern/functions.php
@@ -31,7 +31,7 @@ add_action( 'login_enqueue_scripts', 'login_logo' );
 
 // Custom Favicon
 function favicon_link() {
-    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+    echo '<link rel="shortcut icon" type="image/x-icon" href="'.get_stylesheet_directory_uri().'/assets/img/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'favicon_link' );
 

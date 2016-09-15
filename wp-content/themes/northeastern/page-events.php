c44be4a0390b29b09826e6dc6a17e4d3b6377c3b
Brandon <brandonkfrye@gmail.com>
2016-09-15 17:45:11 -0400

    updated events

--- wp-content/themes/northeastern/page-events.php
+++ wp-content/themes/northeastern/page-events.php
@@ -10,93 +10,98 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div id="event_title">
-			<?php the_title('<h1>','</h1>');?>
-		</div>
-		<div class="event_tags">
-			<div class="event_tags_group" data-filter-group="events">
-				<div id="reset-filter" class="filter-btn is-checked" data-filter="">All Communities</div>
-			<?php
-				$args = array(
-				    'type' => 'events',
-				 );
-
-				$categories = get_categories( $args );
-				foreach($categories as $cat){
-					echo '<div class="filter-btn" data-filter=".'.str_replace(array(' ','.',', '), '-', $cat->name).'">'.$cat->name.'</div>';
-				}
-
-			?>
+		<section class="banner_interior" style="background-image:url(<?php echo get_field('hero_image');?>)">
+			<div class="banner_content">
+				<div class="page_title">
+					<?php the_title('<h1>','</h1>');?>
+				</div>
 			</div>
-		</div>
+		</section>
 
-		<section id="events">
-			<?php
-				$args = array(
-					'post_type' => 'events',
-					'orderby' => 'meta_value_num',
-					'order' => ASC,
-					'meta_key' => 'date_start',
-					'posts_per_page' => '-1'
-				);
-
-				$events = new WP_Query( $args );
-				while ( $events->have_posts() ) : $events->the_post();
-
-					$start = get_field('date_start');
-					$end = get_field('date_end');
-
-					$start_date = strtotime($start);
-					$finish_date = strtotime($end);
-
-					$post_cats = get_the_category();
-
-					if ( $post_cats ) {
-						foreach( $post_cats as $cat ) {
-							$event_cat = $cat->name;
-						}
-					}
-			?>
-
-			<article class="event-item <?php echo str_replace(' ', '-', $event_cat) ?>">
-				<p class="event_info-date">
-					<?php
-					if ( $finish_date == '' ){
-						echo date( 'M j', $start_date );
-					} else {
-						if ( date( 'Y', $start_date ) == date( 'Y', $finish_date ) ){
-							if ( date( 'M', $start_date ) == date( 'M', $finish_date ) ){
-								echo date( 'j', $start_date ) . ' - ' .  date( 'M j', $finish_date );
-							} else {
-								echo date( 'M j', $start_date ) . ' - ' .  date( 'M j', $finish_date );
-							}
-						} else {
-							echo date( 'M j', $start_date ) . ' - ' .  date( 'M j', $finish_date );
-						}
-					}
-					?></p>
-				<p class="event_info-title"><?php echo the_title(); ?><br>
-					<?php
-						echo get_field('start_time');
-						$end_time = get_field('end_time');
-
-						if ( $end_time ){
-							echo ' - ' . $end_time;
-						} else{
-							echo '';
-						}
-
-					?>
+		<div class="events_wrapper">
+			<aside class="events_filtering">
+				<p>
+					FILTER BY
 				</p>
-				<div class="event_info-desc">
-					<p><?php echo get_field('desc') ?></p>
+				<p>
+					Category
+				</p>
+				<select name="cat_filter">
+
+				</select>
+				<p>
+					Type
+				</p>
+				<select name="tag_filter">
+
+				</select>
+			</aside>
+
+			<section id="events">
+				<?php
+					$args = array(
+						'post_type' => 'events',
+						'orderby' => 'meta_value_num',
+						'order' => ASC,
+						'meta_key' => 'date_start',
+						'posts_per_page' => '-1'
+					);
+
+					$events = new WP_Query( $args );
+					while ( $events->have_posts() ) : $events->the_post();
+
+						$start = get_field('date_start');
+						$end = get_field('date_end');
+
+						$start_date = strtotime($start);
+						$finish_date = strtotime($end);
+
+						$post_cats = get_the_category();
+
+						if ( $post_cats ) {
+							foreach( $post_cats as $cat ) {
+								$event_cat = $cat->name;
+							}
+						}
+				?>
+
+				<div class="event-item <?php echo str_replace(' ', '-', $event_cat) ?>">
+					<div class="event_info-wrap">
+                        <div class="event_info-date">
+    						<p>
+    						<?php
+    							echo date( 'M', $start_date );
+								echo '<br>';
+								echo date( 'j', $start_date );
+    						?>
+    						</p>
+    					</div>
+					</div>
+					<div class="event_desc-wrap">
+						<p class="event_info-title"><?php echo the_title(); ?></p>
+						<p class="event_info-details">
+							<?php
+								echo $event_cat . '  |  ';
+								echo get_field('start_time');
+								$end_time = get_field('end_time');
+
+								if ( $end_time ){
+									echo ' - ' . $end_time;
+								} else{
+									echo '';
+								}
+							?>
+						</p>
+						<div class="event_info-desc">
+							<p><?php echo get_field('desc') ?></p>
+						</div>
+					</div>
+					<a href="<?php echo get_field('register') ?>" class="btn_red">Register Now</a>
 				</div>
 
-				<a href="<?php echo get_field('register') ?>" class="btn_red">Register Now</a>
-			</article>
-
-			<?php endwhile; ?>
-		</section>
+				<?php endwhile; ?>
+			</section>
+		</div>
 	</main>
 </div>
 

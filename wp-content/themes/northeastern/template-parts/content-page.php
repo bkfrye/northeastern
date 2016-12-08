0fc886b22d00a9f9b72a6af1e283d263dab84f64
Brandon <brandonkfrye@gmail.com>
2016-12-08 15:02:40 -0500

    changed sidebar fb copy

--- wp-content/themes/northeastern/template-parts/content-page.php
+++ wp-content/themes/northeastern/template-parts/content-page.php
@@ -45,8 +45,29 @@
 				if ( in_array('events', $sidebar_items) ) {
 					get_template_part('template-parts/sidebar', 'events');
 				}
-				if ( in_array('facebook', $sidebar_items) ) {
-					get_template_part('template-parts/sidebar', 'facebook');
+				if ( in_array('facebook', $sidebar_items) ) {?>
+					<div class="facebook_connect">
+					    <div class="facebook_connect-wrap">
+					        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+					        	 viewBox="73 0 267 266.9" enable-background="new 73 0 267 266.9" xml:space="preserve">
+					        <path id="Blue_1_" fill="#3B5998" d="M321.1,262.3c7.9,0,14.2-6.4,14.2-14.2V18.8c0-7.9-6.4-14.2-14.2-14.2H91.8
+					        	C84,4.6,77.6,11,77.6,18.8v229.3c0,7.9,6.4,14.2,14.2,14.2H321.1z"/>
+					        <path id="f" fill="#FFFFFF" d="M255.4,262.3v-99.8h33.5l5-38.9h-38.5V98.8c0-11.3,3.1-18.9,19.3-18.9l20.6,0V45
+					        	c-3.6-0.5-15.8-1.5-30-1.5c-29.7,0-50,18.1-50,51.4v28.7h-33.6v38.9h33.6v99.8H255.4z"/>
+					        </svg>
+					        <p>Connect with community members from your area by joining the NU Alumni Community Facebook Page!</p>
+					    </div>
+					    <div class="btn_wrap">
+					        <?php
+					        if(get_field('facebook_community_link')){
+					            echo '<a href="' . get_field('facebook_community_link') . '" class="btn_red" target="_blank">Follow Us</a>';
+					        }else{
+					            echo '<a href="https://www.facebook.com/NortheasternAlumni/" class="btn_red" target="_blank">Follow Us</a>';
+					        }
+					        ?>
+					    </div>
+					</div>
+		<?php
 				}
 			}
 
